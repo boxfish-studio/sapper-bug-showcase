002a82da0e71d93f2ab9b76f78c39537b7ea0b5e
pedro199288 <pmonteagudo@boxfish.studio>
2020-05-25 17:05:15 +0200

    First Commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,17 @@
+<script>
+  let divClientHeight,
+    height = 20;
+  $: console.log(divClientHeight);
+</script>
+
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<input type="range" bind:value={height} />
+
+<div
+  style="background-color: blue; color:white; height: {height}px; width: 100px"
+  bind:clientHeight={divClientHeight}>
+  div content
+</div>
